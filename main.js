4652a018e13e9fadcee60b5f1fbef79a46d2ed02
Moses Chidozie <80219224+dozeman-moses@users.noreply.github.com>
2021-12-09 23:54:33 +0100

    Update codes

---
+++ main.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+    const current = document.querySelector('.current') ;
+
+    current.classList.remove('current');
+
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add('current');
+    } else {
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+    const current = document.querySelector('.current');
+
+    current.classList.remove('current');
+
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add('current');
+    } else {
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+
+//Button listener
+next.addEventListener('click', e => {
+    nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime)
+    }
+});
+
+prev.addEventListener('click', e => {
+    prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(prevSlide, intervalTime)
+    }
+});
+
+//Auto slide
+if (auto) {
+    //Run next slide at interval time
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
